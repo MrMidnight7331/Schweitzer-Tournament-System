237126bedf638a8a51c35904672efcc06419f3fd
MrMidnight7331 <mr.midnight.7331@gmail.com>
2023-12-14 21:30:41 +0100

    updated shit

--- main.py
+++ main.py
@@ -1,46 +1,46 @@
-"""
-@Project: Schweitzer-System
-@Author: Tieno
-@Version: 6.9
-"""
-
 import random
+
 
 def initialize_tournament(players):
     tournament = {player: 0 for player in players}
     return tournament
 
+
 def print_round_info(round_num, tournament, watching_player):
     print(f"\nRound {round_num} - Current Points:")
     for player, points in tournament.items():
         print(f"{player}: {points} points")
-    
+
     print("\nWatching:")
     print(watching_player)
+
 
 def print_matchups(matchups):
     print("\nMatchups:")
     for match in matchups:
         print(f"{match[0]} vs {match[1]}")
 
+
 def generate_matchups(players):
     random.shuffle(players)
-    matchups = [(players[i], players[i + 1]) for i in range(0, len(players)-1, 2)]
+    matchups = [(players[i], players[i + 1]) for i in range(0, len(players) - 1, 2)]
     return matchups
+
 
 def conduct_round(round_num, tournament, matchups, watching_player):
     print_round_info(round_num, tournament, watching_player)
     print_matchups(matchups)
-    
+
     while True:
         results = {}
         for match in matchups:
-            winner = input(f"Enter the winner of {match[0]} vs {match[1]} (0 for {match[0]}, 1 for {match[1]}, 2 for draw): ").strip()
+            winner = input(
+                f"Enter the winner of {match[0]} vs {match[1]} (0 for {match[0]}, 1 for {match[1]}, 2 for draw): ").strip()
             if winner not in ['0', '1', '2']:
                 print("Invalid input. Try again.")
                 continue
             results[match] = match[int(winner)] if winner != '2' else 'draw'
-        
+
         # Update tournament based on results
         for match, winner in results.items():
             if winner == 'draw':
@@ -48,36 +48,45 @@ def conduct_round(round_num, tournament, matchups, watching_player):
                     tournament[player] += 1
             else:
                 tournament[winner] += 1
-        
+
         break
-    
+
     return tournament
+
 
 def filter_players_by_points(tournament, points):
     return [player for player, p in tournament.items() if p == points]
 
+
 def generate_next_round_matchups(tournament, previous_watching_players):
     eligible_players = set(tournament.keys()) - set(previous_watching_players)
-    
-    if not eligible_players:
-        return []  # No more matchups can be generated
-    
+
+    if len(eligible_players) < 2:
+        return [], None  # No more matchups can be generated
+
     watching_player = random.choice(list(eligible_players))
-    
+    eligible_players.remove(watching_player)
+
+    if len(eligible_players) % 2 != 0:
+        eligible_players.add(watching_player)
+        watching_player = None
+
     next_round_matchups = []
     unique_points = set(tournament.values())
-    
+
     for points in unique_points:
         players_with_same_points = filter_players_by_points(tournament, points)
         if watching_player in players_with_same_points:
             players_with_same_points.remove(watching_player)
         next_round_matchups.extend(generate_matchups(players_with_same_points))
-    
+
     random.shuffle(next_round_matchups)
     return next_round_matchups, watching_player
 
+
 def main():
-    print(" _____                                                           _   \n(_   _)                                                         ( )_ \n  | |   _    _   _  _ __   ___     _ _   ___ ___     __    ___  | ,_)\n  | | /'_`\ ( ) ( )( '__)/' _ `\ /'_` )/' _ ` _ `\ /'__`\/' _ `\| |  \n  | |( (_) )| (_) || |   | ( ) |( (_| || ( ) ( ) |(  ___/| ( ) || |_ \n  (_)`\___/'`\___/'(_)   (_) (_)`\__,_)(_) (_) (_)`\____)(_) (_)`\__)")
+    print(
+        " _____                                                           _   \n(_   _)                                                         ( )_ \n  | |   _    _   _  _ __   ___     _ _   ___ ___     __    ___  | ,_)\n  | | /'_`\ ( ) ( )( '__)/' _ `\ /'_` )/' _ ` _ `\ /'__`\/' _ `\| |  \n  | |( (_) )| (_) || |   | ( ) |( (_| || ( ) ( ) |(  ___/| ( ) || |_ \n  (_)`\___/'`\___/'(_)   (_) (_)`\__,_)(_) (_) (_)`\____)(_) (_)`\__)")
     print("By: Tieno / Version: 6.9\n")
     player_names = input("Enter player names separated by commas: ").split(',')
     tournament = initialize_tournament(player_names)
@@ -86,7 +95,7 @@ def main():
     round_num = 1
     while True:
         matchups, watching_player = generate_next_round_matchups(tournament, watching_players)
-        
+
         if not matchups:
             break  # Tournament ends when there are no more matchups to generate
 
@@ -96,6 +105,7 @@ def main():
 
     print("\nTournament Complete!")
     print_round_info(round_num - 1, tournament, "None")
+
 
 if __name__ == "__main__":
     main()
